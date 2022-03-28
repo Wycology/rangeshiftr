a7444d74fab0ad77b1926b6650f389d3bccca71c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-03-28 15:28:03 +0200

    c

--- scripts/tutorial_1.R
+++ scripts/tutorial_1.R
@@ -7,7 +7,7 @@ library(viridis)
 library(grid)
 library(gridExtra)
 
-# relative path from working 
+# relative path from workin
 dirpath = "Tutorial_01/"
 
 dir.create(paste0(dirpath,'Inputs'), showWarnings = TRUE)
