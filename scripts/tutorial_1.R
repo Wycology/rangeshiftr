8cf6de40c8d700b2c8108cd5d35c06b2bca6e96d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-07 19:17:50 +0200

    Mapping shifting ranges of species

--- scripts/tutorial_1.R
+++ scripts/tutorial_1.R
@@ -19,7 +19,7 @@ dir.create(paste0(dirpath,"Output_Maps"), showWarnings = TRUE) # Creating folder
 # Here is the path to original data https://rangeshifter.github.io/RangeshiftR-tutorials/files/Tutorial1_Inputs.zip
 # The data is to be downloaded and stored in the Inputs folder.
 
-UKmap <- raster(paste0(dirpath, "Inputs/UKmap_1km.txt"))
+UKmap <- raster(paste0(dirpath, "Inputs/UKmap_1km.txt")) # Loading data 
 SpDist <- raster(paste0(dirpath, "Inputs/Species_Distribution_10km.txt"))
 values(SpDist)[values(SpDist) < 1] <- NA
 
