abad32ae7ae42523a254bb9e815bf9d30fe08237
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-10 21:49:03 +0200

    Boosted regreesion trees in R

--- brt_in_R.R
+++ brt_in_R.R
@@ -2,4 +2,39 @@
 
 library(MASS) # Library with the data to work on
 data("Boston")
-str(Boston)
+library(gbm) # Library for building the model
+
+model <- gbm(medv ~., data = Boston)
+model
+
+library(caret)
+set.seed(1)
+
+model <- train(medv ~., data = Boston, method = "gbm", verbose = FALSE)
+
+model
+
+plot(model)
+
+set.seed(1)
+
+model2 <- train(medv ~., data = Boston, method = "gbm", 
+                preProcess = c("center", "scale"), verbose = FALSE)
+model2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
