e919006fb587b2e65e984c05afc606c17157e27b
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-23 22:20:32 +0200

    rangeshiftR reformating

--- scripts/tutorial_1.R
+++ scripts/tutorial_1.R
@@ -33,7 +33,7 @@ plot(rasterToPolygons(SpDist, dissolve = F), add = T)
 
 UKmap.f <- as.factor(UKmap)
 
-# Add the land cover classes to the raster attributes table (ra
+# Add the land cover classes to the raster attributes table (r
 
 rat <- levels(UKmap.f)[[1]]
 
