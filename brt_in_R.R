9260ca43ab9f101d226e8e607631e5cf19312217
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-07-13 14:27:34 +0200

    RangeshiftR

--- brt_in_R.R
+++ brt_in_R.R
@@ -47,7 +47,7 @@ test_target <- subset(testing, select = medv)[,1]
 
 predictions <- predict(model3, newdata = test_features)
 
-# RMSE That is Roome Means Square Error
+# RMSE That is Room Means Square Error
 
 sqrt(mean((test_target - predictions)^2))
 
