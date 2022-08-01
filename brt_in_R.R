dce57c890f3b9e82a8c03e110dc3173643a31aa9
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-08-01 20:08:25 +0200

    Working with brt in R

--- brt_in_R.R
+++ brt_in_R.R
@@ -3,7 +3,7 @@
 
 # Boosted regression trees in R
 
-library(MASS) # This is thlibrary with the data to work on
+library(MASS) # This is tlibrary with the data to work on
 data("Boston") # Loading the dataset
 library(gbm) # Library for building the boosted regression model
 library(caret)
