19dfb028f8ed78df0acdb5d257810a99ab007eec
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-08-09 16:51:07 +0200

    Working with rangeshiftr

--- brt_in_R.R
+++ brt_in_R.R
@@ -3,7 +3,7 @@
 
 # Boosted regression trees in R
 
-library(MASS) # Library with the data to wor
+library(MASS) # Library with the data to wo
 data("Boston") # Loading the dataset
 library(gbm) # Library for building the boosted regression model
 library(caret)
