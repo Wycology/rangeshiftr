d1df80c5d0bf68079ff8c1280a37c5607d398b46
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-08-17 17:04:53 +0200

    Brt in R

--- brt_in_R.R
+++ brt_in_R.R
@@ -39,7 +39,7 @@ set.seed(1)
 model3 <- train(medv ~., data = training, method = "gbm", 
                 preProcess = c("center", "scale"), verbose = FALSE)
 
-model3 # The model output
+model3 # The model output p
 
 test_features <- subset(testing, select = -c(medv))
 test_target <- subset(testing, select = medv)[,1]
