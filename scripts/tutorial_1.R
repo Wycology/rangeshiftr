3ca462cf26519339308f16592e856c19fa217707
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-09-07 21:04:09 +0200

    Working with Rangeshifter

--- scripts/tutorial_1.R
+++ scripts/tutorial_1.R
@@ -1,4 +1,4 @@
-# Loadin the libraries for the task of mapping range shifts.
+# Loadi the libraries for the task of mapping range shifts.
 
 library(RangeShiftR)    # version 1.0.3
 library(raster)         # version 3.5.29
